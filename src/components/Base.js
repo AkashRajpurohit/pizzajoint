e37a9d91ae401d41583a5d7b39a97a296d7d5e7a
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2020-07-12 21:52:49 +0530

    :sparkles: hover animations

--- src/components/Base.js
+++ src/components/Base.js
@@ -17,9 +17,16 @@ const Base = ({ addBase, pizza }) => {
         {bases.map(base => {
           let spanClass = pizza.base === base ? 'active' : '';
           return (
-            <li key={base} onClick={() => addBase(base)}>
+            <motion.li key={base} onClick={() => addBase(base)}
+              whileHover={{ 
+                scale: 1.3,
+                color: '#f8e112',
+                originX: 0
+              }}
+              transition={{ type: 'spring', stiffness: 300 }}
+            >
               <span className={spanClass}>{ base }</span>
-            </li>
+            </motion.li>
           )
         })}
       </ul>
@@ -31,7 +38,15 @@ const Base = ({ addBase, pizza }) => {
           transition={{ type: 'spring', stiffness: 120 }}
         >
           <Link to="/toppings">
-            <button>Next</button>
+            <motion.button
+              whileHover={{ 
+                scale: 1.1,
+                textShadow: '0px 0px 8px rgb(255,255,255)',
+                boxShadow: '0px 0px 8px rgb(255,255,255)'
+              }}
+            >
+              Next
+            </motion.button>
           </Link>
         </motion.div>
       )}
