58b39965d6a68f0c3c40c095f542a2b9d2bd3d87
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2020-07-14 21:48:27 +0530

    :fire: variants

--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -2,11 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { 
+      type: 'spring', 
+      delay: 0.5
+    }
+  }
+}
+
 const Toppings = ({ addTopping, pizza }) => {
   let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
 
   return (
-    <div className="toppings container">
+    <motion.div className="toppings container"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
       
       <h3>Step 2: Choose Toppings</h3>
       <ul>
@@ -39,7 +58,7 @@ const Toppings = ({ addTopping, pizza }) => {
         </motion.button>
       </Link>
 
-    </div>
+    </motion.div>
   )
 }
 
