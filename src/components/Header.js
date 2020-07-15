bc6df378dadf1d1c7676f3a13ad196e80050fa62
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2020-07-15 19:56:18 +0530

    :sparkles: svg animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,50 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const svgVariants = {
+  initial: {
+    rotate: -180
+  },
+  animate: {
+    rotate: 0,
+    transition: {
+      duration: 1
+    }
+  }
+}
+
+const pathVariants = {
+  initial: {
+    opacity: 0,
+    pathLength: 0
+  },
+  animate: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 2,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 const Header = () => {
   return (
     <Link to="/">
       <header>
         <div className="logo">
-          <svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-            <path
+          <motion.svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" variants={svgVariants} initial="initial" animate="animate">
+            <motion.path
               fill="none"
               d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+              variants={pathVariants}
             />
-            <path
+            <motion.path
               fill="none"
               d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+              variants={pathVariants}
             />
-          </svg>
+          </motion.svg>
         </div>
         <motion.div className="title"
           animate={{ y: -10 }}
